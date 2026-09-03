6e792fce9e8ab4b5ebc5005fe9a0beadeec7438c
Juan Orlandini <tachijuan@gmail.com>
2026-09-03 11:22:42 -0400

    myd 1.4.0

--- Formula/myd.rb
+++ Formula/myd.rb
@@ -1,8 +1,8 @@
 class Myd < Formula
   desc "Vi-like terminal file browser with size bars, treemap, archives and SFTP"
   homepage "https://github.com/tachijuan/myd"
-  url "https://github.com/tachijuan/myd/archive/refs/tags/v1.3.9.tar.gz"
-  sha256 "25f3f62ca5a25a04ca18cab299784a246aa79e47ac8064c4fc3e38d672abaf53"
+  url "https://github.com/tachijuan/myd/archive/refs/tags/v1.4.0.tar.gz"
+  sha256 "01da34c43c9b5d55d4dd537008de7d00cbd57792e385ed0526b9d9485af23660"
   license "MIT"
   head "https://github.com/tachijuan/myd.git", branch: "master"
 
